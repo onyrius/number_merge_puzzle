2894818ad8708997b6720936937b4c4de88a5ec4
onyrius <onyrius@users.noreply.github.com>
2026-06-22 20:01:28 +0000

    deploy: 029ab9301144ff0e1b8c1459ba298c2e718914ac

--- main.dart.js
+++ main.dart.js
@@ -21234,7 +21234,7 @@ $iCM:1}
 A.a8F.prototype={
 $1(a){var s=A.ci().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
 $S:105}
 A.Fu.prototype={
 gzq(){var s=this.b
